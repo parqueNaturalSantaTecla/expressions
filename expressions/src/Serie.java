383c9c307b8e389f539b1f1563cf00cd4c13c6e5
Tamara <Tamara@192.168.0.11>
2019-07-10 12:11:45 +0200

    A. Creada clase Serie

---
+++ expressions/src/Serie.java
@@ -0,0 +1,28 @@
+
+
+public class Serie {
+
+	final int MAX = 10;
+
+	int getTerm(int number) {
+//		return number; // en orden
+//		return number + 1; // en orden del 1 al 10
+//		return (MAX-1) - number; // en orden inverso del 0 al 9
+//		return number % 3; // serie de 0,1,2
+//		return number * 2; // de 2 en 2
+//		return (number + 1) * MAX; // la tabla del 10
+		return ((MAX - number - 1) / (MAX / 2)) * number + (number / (MAX / 2)) * (MAX - number - 1);
+	}
+
+	void exec() {
+		for (int i = 0; i < MAX; i++) {
+			System.out.println(i + " - " + getTerm(i));
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		new Serie().exec();
+	}
+
+}
