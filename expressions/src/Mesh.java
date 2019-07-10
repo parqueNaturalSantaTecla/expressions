544002a0617d3a634ef9f0dc1435ea0acff66b0c
setil <setil@DESKTOP-PEHM5QR>
2019-07-10 13:09:04 +0200

    R. Recolocación de métodos

--- expressions/src/Mesh.java
+++ expressions/src/Mesh.java
@@ -1,38 +1,11 @@
+class Mesh {
 
-
-public class Mesh {
+	public static void main(String[] args) {
+		new Mesh().exec();
+	}
 
 	final int MAX = 15;
 	
-	boolean condition(int row, int column) {
-		return row==0 && column==0; // punto en esquina superior izquierda
-//		return row==3 && column==12; // punto particular
-//		return row==MAX/2 && column==MAX/2; // punto central
-
-//		return row==MAX/2; // fila
-//		return column==MAX/2; // columna
-//		return row==MAX/2 || column==MAX/2; // cruz centrada
-//		return row==7 || column==12; // cruz descentrada
-		
-//		return row==0 || row==MAX-1 || column==0 || column==MAX-1; // marco externo
-//		return row==1 || row==MAX-2 || column==1 || column==MAX-2; // marco interno	
-
-//		return 5 <= row && row <= 10 && 5 <= column && column <= 10; // cuadro interno
-	
-//		return row - column == 0; // diagonal principal
-//		return row + column == MAX - 1; // diagonal inversa
-//		return row - column == 5; // paralela a diagonal principal
-//		return row + column == 5; // paralela a diagonal inversa
-//		return row - column == 0 || row + column == MAX - 1; // aspa
-//		return row - column == 3 || row + column == MAX - 3; // aspa desplazada
-		
-//		return row == MAX / 2 || column == MAX / 2 || row - column == 0 || row + column == MAX - 1; // ikurriña
-		
-//		return (row+column)%2==0; // alternos
-//		return (row+column)%2==1; // alternos
-		
-	}
-
 	void exec() {
 		System.out.print(" ");
 		for (int j = 0; j < MAX; j++) {
@@ -52,8 +25,26 @@ public class Mesh {
 		}
 	}
 	
-	public static void main(String[] args) {
-		new Mesh().exec();
+	boolean condition(int row, int column) {
+		return row==0 && column==0; // punto en esquina superior izquierda
+//		return row==3 && column==12; // punto particular
+//		return row==MAX/2 && column==MAX/2; // punto central
+//		return row==MAX/2; // fila
+//		return column==MAX/2; // columna
+//		return row==MAX/2 || column==MAX/2; // cruz centrada
+//		return row==7 || column==12; // cruz descentrada
+//		return row==0 || row==MAX-1 || column==0 || column==MAX-1; // marco externo
+//		return row==1 || row==MAX-2 || column==1 || column==MAX-2; // marco interno	
+//		return 5 <= row && row <= 10 && 5 <= column && column <= 10; // cuadro interno
+//		return row - column == 0; // diagonal principal
+//		return row + column == MAX - 1; // diagonal inversa
+//		return row - column == 5; // paralela a diagonal principal
+//		return row + column == 5; // paralela a diagonal inversa
+//		return row - column == 0 || row + column == MAX - 1; // aspa
+//		return row - column == 3 || row + column == MAX - 3; // aspa desplazada
+//		return row == MAX / 2 || column == MAX / 2 || row - column == 0 || row + column == MAX - 1; // ikurriña
+//		return (row+column)%2==0; // alternos
+//		return (row+column)%2==1; // alternos
 	}
 	
 }
